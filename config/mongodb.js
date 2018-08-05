dc32ffe35af8d309ebff623908a1f76f907343c7
Akash Agarwal <akash.kumar@axisrooms.com>
2018-08-05 21:37:27 +0530

    mongodb connection

--- config/mongodb.js
+++ config/mongodb.js
@@ -3,8 +3,8 @@ var mongoose = require('mongoose');
 //Set up MongoDb connection
 function _init(){
     try{
-        return mongoose.createConnection('mongodb://localhost:27017/abacus');
-        //return mongoose.createConnection('mongodb://han_solo:chewbacca@ds011419.mlab.com:11419/mean-boilerplate-test');
+        // return mongoose.createConnection('mongodb://localhost:27017/abacus');
+        return mongoose.createConnection('mongodb://admin:Abcd123$0@ds213612.mlab.com:13612/heroku_49cdczxx');
     }catch(err){
         console.log("No internet connection :(");
     }
