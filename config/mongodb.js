424df854f7e878a7a5a27b460b2abb518be0a7b4
Akash Agarwal <akash.kumar@axisrooms.com>
2018-09-05 01:31:34 +0530

    Connect to Prod

--- config/mongodb.js
+++ config/mongodb.js
@@ -3,8 +3,14 @@ var mongoose = require('mongoose');
 //Set up MongoDb connection
 function _init(){
     try{
-        return mongoose.createConnection('mongodb://localhost:27017/abacus');
+        // Local
+        // return mongoose.createConnection('mongodb://localhost:27017/abacus');
+        
+        // Stag
         // return mongoose.createConnection('mongodb://admin:Abcd123$0@ds213612.mlab.com:13612/heroku_49cdczxx');
+
+        // Live
+        return mongoose.createConnection('mongodb://admin:Abcd123$0@ds145072.mlab.com:45072/heroku_8t0b2wbr');
     }catch(err){
         console.log("No internet connection :(");
     }
