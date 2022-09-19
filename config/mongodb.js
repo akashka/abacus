4b8588ba487f7c74a3614791a686bd0c4087942e
Akash Agarwal <30892454+akashka@users.noreply.github.com>
2022-09-19 19:23:46 +0530

    Update mongodb.js

--- config/mongodb.js
+++ config/mongodb.js
@@ -10,7 +10,7 @@ function _init(){
         // return mongoose.createConnection('mongodb://admin:Abcd123$0@ds213612.mlab.com:13612/heroku_49cdczxx');
 
         // Live
-        return mongoose.createConnection('mongodb://akash:akash@ac-ao8t6fk-shard-00-00.shyoqs4.mongodb.net:27017,ac-ao8t6fk-shard-00-01.shyoqs4.mongodb.net:27017,ac-ao8t6fk-shard-00-02.shyoqs4.mongodb.net:27017/test?replicaSet=atlas-fblq6e-shard-0&ssl=true&authSource=admin
+        return mongoose.createConnection('mongodb://akash:akash@ac-ao8t6fk-shard-00-00.shyoqs4.mongodb.net:27017,ac-ao8t6fk-shard-00-01.shyoqs4.mongodb.net:27017,ac-ao8t6fk-shard-00-02.shyoqs4.mongodb.net:27017/aloha-abacus?replicaSet=atlas-fblq6e-shard-0&ssl=true&authSource=admin
 ');
     }catch(err){
         console.log("No internet connection :(");
