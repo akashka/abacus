7965489b995f2235a8394e4c905e2b407114f421
Akash Agarwal <30892454+akashka@users.noreply.github.com>
2022-09-19 19:37:49 +0530

    Update mongodb.js

--- config/mongodb.js
+++ config/mongodb.js
@@ -10,7 +10,7 @@ function _init(){
         // return mongoose.createConnection('mongodb://admin:Abcd123$0@ds213612.mlab.com:13612/heroku_49cdczxx');
 
         // Live
-        return mongoose.createConnection('mongodb://akash:akash@ac-ao8t6fk-shard-00-00.shyoqs4.mongodb.net:27017/aloha-abacus');
+        return mongoose.createConnection('mongodb://akash:abcd1234@cluster-7bl0f9cp-shard-00-00.txl42.mongodb.net:27017,cluster-7bl0f9cp-shard-00-01.txl42.mongodb.net:27017,cluster-7bl0f9cp-shard-00-02.txl42.mongodb.net:27017/heroku_7bl0f9cp?authSource=admin&replicaSet=atlas-eel26w-shard-0&w=majority%22&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true');
     }catch(err){
         console.log("No internet connection :(");
     }
